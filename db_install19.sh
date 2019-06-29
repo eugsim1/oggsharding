7ffe3cd38dbc643fe06b529374252972889972ed
eugsim1 <22279788+eugsim1@users.noreply.github.com>
2019-06-29 18:14:19 +0300

    Create db_install19.sh

---
+++ db_install19.sh
@@ -0,0 +1,29 @@
+source ~/scripts/oggsharding/ora19.env
+
+## install oracle software
+mkdir -p $ORACLE_HOME
+cd $ORACLE_HOME
+unzip -oq /u01/stage/LINUX.X64_193000_db_home.zip
+
+# Install DB software Silent mode.
+./runInstaller -ignorePrereq -waitforcompletion -silent                        \
+    -responseFile ${ORACLE_HOME}/install/response/db_install.rsp               \
+    oracle.install.option=INSTALL_DB_SWONLY                                    \
+    ORACLE_HOSTNAME=${ORACLE_HOSTNAME}                                         \
+    UNIX_GROUP_NAME=oinstall                                                   \
+    INVENTORY_LOCATION=${ORA_INVENTORY}                                        \
+    SELECTED_LANGUAGES=en,en_GB                                                \
+    ORACLE_HOME=${ORACLE_HOME}                                                 \
+    ORACLE_BASE=${ORACLE_BASE}                                                 \
+    oracle.install.db.InstallEdition=EE                                        \
+    oracle.install.db.OSDBA_GROUP=dba                                          \
+    oracle.install.db.OSBACKUPDBA_GROUP=dba                                    \
+    oracle.install.db.OSDGDBA_GROUP=dba                                        \
+    oracle.install.db.OSKMDBA_GROUP=dba                                        \
+    oracle.install.db.OSRACDBA_GROUP=dba                                       \
+    SECURITY_UPDATES_VIA_MYORACLESUPPORT=false                                 \
+    DECLINE_SECURITY_UPDATES=true
+	
+	
+sudo   /u01/app/oraInventory/orainstRoot.sh
+sudo  /u01/app/oracle/product/19.0.0/dbhome_1/root.sh
