66b1f817f02ba5996fecdc9649612b9e8ad6cf99
eugsim1 <22279788+eugsim1@users.noreply.github.com>
2019-06-30 11:20:10 +0300

    Update db-install-18.c

--- db-install-18.c
+++ db-install-18.c
@@ -50,19 +50,38 @@ sudo   /u01/app/oraInventory/orainstRoot.sh
 sudo  /u01/app/oracle/product/18.0.0/dbhome_1/root.sh	
 
 	
-### clean previous database installations
-source /home/oracle/scripts/ora12.env
-
 dbca -silent -deleteDatabase -sourceDB shardcat -sysDBAUserName sys -sysDBAPassword SysPassword1
 lsnrctl stop
 mv $ORACLE_HOME/network/admin/linstener.ora /home/oracle/scripts/listener.ora-original
 mv $ORACLE_HOME/networdk/admin/tnsnames.ora /home/oracle/scripts/tnsnames.ora-ORIGINAL
 
-netca -silent -responseFile $ORACLE_HOME/assistants/netca/netca.rsp
-lsnrctl status
-lsnrctl start
+#netca -silent -responseFile $ORACLE_HOME/assistants/netca/netca.rsp
+#lsnrctl status
+#lsnrctl start
 
-
+sudo touch /etc/oratab
+sudo chmod ugo+rw /etc/oratab
+sudo chown oracle:oinstall /etc/oratab
+dbca -silent -createDatabase                                                   \
+     -templateName General_Purpose.dbc                                         \
+     -gdbname $server -sid  $server -responseFile NO_VALUE         \
+     -characterSet AL32UTF8                  lab                                  \
+     -sysPassword SysPassword1                                                 \
+     -systemPassword SysPassword1                                              \
+     -createAsContainerDatabase false                                           \
+     -databaseType MULTIPURPOSE                                                \
+     -automaticMemoryManagement false                                          \
+	 -enableArchive true   \
+	 -initParams db_recovery_file_dest=${ORACLE_BASE}/fast_recovery_area/{DB_UNIQUE_NAME}  \
+	 db_recovery_file_dest_size=20G  STREAMS_POOL_SIZE=1200M \
+     -totalMemory 5000                                                         \
+     -storageType FS                                                           \
+     -datafileDestination "${DATA_DIR}"                                        \
+     -redoLogFileSize 50                                                       \
+     -emConfiguration NONE                                                   \
+	 -createListener LISTENER:1521  \
+	 -customScripts init.sql \
+     -ignorePreReqs
 	
 ### install the catalog database
 dbca -silent -createDatabase                                                   \
@@ -79,30 +98,16 @@ dbca -silent -createDatabase                                                   \
      -datafileDestination "${DATA_DIR}"                                        \
      -redoLogFileSize 50                                                       \
      -emConfiguration NONE                                                     \
+     -customScripts initshardcat.sql  \
      -ignorePreReqs	
 	
 	
 ### configuration of the catalog
 
-source /home/oracle/scripts/ora12.env
+
 source /home/oracle/scripts/ora18.env
-source /home/oracle/scripts/ora19.env
-sqlplus / as sysdba<<EOF
-alter system set db_create_file_dest='/u01/app/oracle/oradata' scope=both;
-alter system set open_links=16 scope=spfile;
-alter system set open_links_per_instance=16 scope=spfile;
-shutdown immediate;
-startup;
-set echo on;
-set termout on;
-spool setup_grants_privs.lst;
-alter user gsmcatuser account unlock;
-alter user gsmcatuser identified by Welcome1;
-create user mysdbadmin identified by Welcome1;
-grant connect, create session, gsmadmin_role to mysdbadmin;
-grant inherit privileges on user SYS to GSMADMIN_INTERNAL;
-spool off;
-EOF
+
+sqlplus sys/SysPassword1@shardcat as sysdba<<EOF
 
 
 
