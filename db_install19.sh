e849e274f4f3a143dd85c1361d495049b9781bd6
eugsim1 <22279788+eugsim1@users.noreply.github.com>
2019-06-30 11:15:18 +0300

    Update db_install19.sh

--- db_install19.sh
+++ db_install19.sh
@@ -27,3 +27,29 @@ unzip -oq /u01/stage/LINUX.X64_193000_db_home.zip
 	
 sudo   /u01/app/oraInventory/orainstRoot.sh
 sudo  /u01/app/oracle/product/19.0.0/dbhome_1/root.sh
+
+
+sudo touch /etc/oratab
+sudo chmod ugo+rw /etc/oratab
+sudo chown oracle:oinstall /etc/oratab
+dbca -silent -createDatabase                                                   \
+     -templateName General_Purpose.dbc                                         \
+     -gdbname $server -sid  $server -responseFile NO_VALUE         \
+     -characterSet AL32UTF8                  lab                                  \
+     -sysPassword SysPassword1                                                 \
+     -systemPassword SysPassword1                                              \
+     -createAsContainerDatabase false                                           \
+     -databaseType MULTIPURPOSE                                                \
+     -automaticMemoryManagement false                                          \
+	 -enableArchive true   \
+	 -initParams db_recovery_file_dest=${ORACLE_BASE}/fast_recovery_area/{DB_UNIQUE_NAME}  \
+	 db_recovery_file_dest_size=20G  STREAMS_POOL_SIZE=1200M \
+     -totalMemory 5000                                                         \
+     -storageType FS                                                           \
+     -datafileDestination "${DATA_DIR}"                                        \
+     -redoLogFileSize 50                                                       \
+     -emConfiguration NONE                                                   \
+	 -createListener LISTENER:1521  \
+	 -customScripts init.sql \
+     -ignorePreReqs
+	 
