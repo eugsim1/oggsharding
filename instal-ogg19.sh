49337c68998440df8ffcbde46033bf75e1bc2efc
eugsim1 <22279788+eugsim1@users.noreply.github.com>
2019-07-01 11:43:59 +0300

    Update instal-ogg19.sh

--- instal-ogg19.sh
+++ instal-ogg19.sh
@@ -8,6 +8,7 @@ echo $server
 export ORACLE_HOSTNAME=$server
 export ORACLE_BASE=/u01/app/oracle
 export ORACLE_SID=$server
+export ORA_INVENTORY=/u01/app/oraInventory
 export ORACLE_HOME=$ORACLE_BASE/product/19.0.0/dbhome_1
 export TNS_ADMIN=${ORACLE_HOME}/network/admin
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${ORACLE_HOME}/lib
@@ -18,6 +19,7 @@ export ORACLE_HOME PATH ORACLE_SID TNS_ADMIN LD_LIBRARY_PATH
 export ORACLE_HOSTNAME=$server
 export ORACLE_BASE=/u01/app/oracle
 export ORACLE_SID=$server
+export ORA_INVENTORY=/u01/app/oraInventory
 export ORACLE_HOME=$ORACLE_BASE/product/18.0.0/dbhome_1
 export TNS_ADMIN=${ORACLE_HOME}/network/admin
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${ORACLE_HOME}/lib
