3ef0cd9ede4b51e09881b511605924be4583ea60
eugsim1 <22279788+eugsim1@users.noreply.github.com>
2019-07-08 18:12:10 +0300

    Update install-ogg19.sh

--- install-ogg19.sh
+++ install-ogg19.sh
@@ -45,13 +45,9 @@ for pid in $(ps -ef | grep "oggma" | awk '{print $2}');  do kill -9 $pid; done
 
 
 # remove previous entries from the inventory file
-
-sed '/oggma/d' /u01/app/oraInventory/ContentsXML/inventory.xml > loc.xml
-mv loc.xml /u01/app/oraInventory/ContentsXML/inventory.xml
-cat /u01/app/oraInventory/ContentsXML/inventory.xml
-
-
-
+#sed '/oggma/d' /u01/app/oraInventory/ContentsXML/inventory.xml > loc.xml
+#mv loc.xml /u01/app/oraInventory/ContentsXML/inventory.xml
+#cat /u01/app/oraInventory/ContentsXML/inventory.xml
 
 :'
 ### install ogg ma core software
@@ -79,8 +75,7 @@ which orapki
 serverFQDN=`hostname -f` 
 server=$(echo $serverFQDN | sed 's/\..*//')
 echo $server
-export serverFQDN=$server
-echo $serverFQDN
+
 
 export WALLET_DIR=$ORACLE_BASE/admin/wallet_dir
 export SHARDIND_WALLET_DIR=$ORACLE_BASE/admin/ggshd_wallet
@@ -106,19 +101,19 @@ if [[ $server == "sharddirector" ]]
 fi
 
 
-  ## create serverFQDN certificate for the host
-orapki wallet create -wallet $WALLET_DIR/$serverFQDN -auto_login -pwd Welcome1
-orapki wallet add -wallet $WALLET_DIR/$serverFQDN -dn "CN=$serverFQDN" -keysize 2048 -pwd Welcome1
-orapki wallet export -wallet $WALLET_DIR/$serverFQDN -pwd Welcome1  -dn "CN=$serverFQDN"  -request $WALLET_DIR/${serverFQDN}_req.pem
-orapki cert create -wallet $WALLET_DIR/root_ca -request $WALLET_DIR/${serverFQDN}_req.pem -cert $WALLET_DIR/${serverFQDN}_Cert.pem -serial_num 20 -validity 365 -pwd Welcome1  -sign_alg sha256
-orapki wallet add -wallet $WALLET_DIR/$serverFQDN -trusted_cert -cert $WALLET_DIR/rootCA_Cert.pem -pwd Welcome1
-orapki wallet add -wallet $WALLET_DIR/$serverFQDN -user_cert  -cert $WALLET_DIR/${serverFQDN}_Cert.pem -pwd Welcome1
+## create server certificate for the host short name not FQDN
+orapki wallet create -wallet $WALLET_DIR/$server -auto_login -pwd Welcome1
+orapki wallet add -wallet $WALLET_DIR/$server -dn "CN=$server" -keysize 2048 -pwd Welcome1
+orapki wallet export -wallet $WALLET_DIR/$server -pwd Welcome1  -dn "CN=$server"  -request $WALLET_DIR/${server}_req.pem
+orapki cert create -wallet $WALLET_DIR/root_ca -request $WALLET_DIR/${server}_req.pem -cert $WALLET_DIR/${server}_Cert.pem -serial_num 20 -validity 365 -pwd Welcome1  -sign_alg sha256
+orapki wallet add -wallet $WALLET_DIR/$server -trusted_cert -cert $WALLET_DIR/rootCA_Cert.pem -pwd Welcome1
+orapki wallet add -wallet $WALLET_DIR/$server -user_cert  -cert $WALLET_DIR/${server}_Cert.pem -pwd Welcome1
 ### display wallet configuration
-orapki wallet display -wallet $WALLET_DIR/$serverFQDN -pwd Welcome1
+orapki wallet display -wallet $WALLET_DIR/$server -pwd Welcome1
 ### create a distribution server user certificate
 orapki wallet create -wallet $WALLET_DIR/dist_client -auto_login -pwd Welcome1
-orapki wallet add -wallet $WALLET_DIR/dist_client -dn "CN=$serverFQDN" -keysize 2048 -pwd Welcome1
-orapki wallet export -wallet $WALLET_DIR/dist_client -pwd Welcome1  -dn "CN=$serverFQDN"  -request $WALLET_DIR/dist_client_req.pem
+orapki wallet add -wallet $WALLET_DIR/dist_client -dn "CN=$server" -keysize 2048 -pwd Welcome1
+orapki wallet export -wallet $WALLET_DIR/dist_client -pwd Welcome1  -dn "CN=$server"  -request $WALLET_DIR/dist_client_req.pem
 orapki cert create -wallet $WALLET_DIR/root_ca -request $WALLET_DIR/dist_client_req.pem -cert $WALLET_DIR/dist_client_Cert.pem -serial_num 30 -validity 365 -pwd Welcome1
 orapki wallet add -wallet $WALLET_DIR/dist_client -trusted_cert -cert $WALLET_DIR/rootCA_Cert.pem -pwd Welcome1
 orapki wallet add -wallet $WALLET_DIR/dist_client -user_cert  -cert $WALLET_DIR/dist_client_Cert.pem -pwd Welcome1
@@ -138,17 +133,21 @@ orapki wallet add -wallet $SHARDIND_WALLET_DIR -user_cert  -cert $SHARDIND_WALLE
 orapki wallet display -wallet $SHARDIND_WALLET_DIR
 
 
-
+### deployement of the oggma 
 
 export OGG_BASE=/u01/app/ogg
 export OGG_HOME=/u01/app/ogg/oggma
 export OGG_BIN=/u01/app/ogg/oggbin
-server=`hostname -f`
+serverFQDN=`hostname -f` 
+server=$(echo $serverFQDN | sed 's/\..*//')
+echo $server
 for pid in $(ps -ef | grep "oggma" | awk '{print $2}');  do kill -9 $pid; done
 rm -rf /u01/app/ogg/oggma_first /u01/app/ogg/oggma_deploy
+
 cd ${OGG_HOME}/bin
 ./oggca.sh -silent -responseFile  ~/scripts/oggsharding/oggca19.rsp HOST_SERVICEMANAGER=$server \
-SERVER_WALLET=/u01/app/oracle/admin/wallet_dir/$server
+SERVER_WALLET=$WALLET_DIR/$server CLIENT_WALLET=$WALLET_DIR/dist_client
+
 
 
 
@@ -158,7 +157,6 @@ export ORACLE_BASE=/u01/app/oracle
 export ORACLE_SID=$server
 export ORACLE_HOME=$ORACLE_BASE/product/19.0.0/dbhome_1
 export TNS_ADMIN=${ORACLE_HOME}/network/admin
-
 export OGG_BASE=/u01/app/ogg
 export OGG_HOME=/u01/app/ogg/oggma
 export OGG_BIN=/u01/app/ogg/oggbin
@@ -167,7 +165,6 @@ export PATH=$OGG_HOME/bin:$OGG_HOME/jdk/bin:$ORACLE_HOME/bin:$PATH
 
 export OGG_ETC_HOME=/u01/app/ogg/oggma_first/etc
 export OGG_VAR_HOME=/u01/app/ogg/oggma_first/var
-
 export LD_LIBRARY_PATH=$ORACLE_HOME/lib:$LD_LIBRARY_PATH
 
 
